9c568330ae3651b3fb1d1adb38544d94a3a5143e
Veronicah <veronicah.gicheru@student.moringaschool.com>
2022-06-07 16:29:00 -0700

    commit tree a93c4e7fd730cf386bab735cbd519c7808f8357e

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // Code your solution in this file!
+function returnFirstTwoDrivers(drivers){
+    return drivers.slice(0,2)
+    }
+    function returnLastTwoDrivers(drivers){
+        return drivers.slice(-2)
+    }
+    const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers ]
+
+    const createFareMultiplier = function (multiplier){
+        return function (fare){
+            return multiplier * fare
+    } 
+}
+const fareDoubler = createFareMultiplier (2)
+
+const fareTripler = createFareMultiplier (3)
+
+function selectDifferentDrivers(arrayOfDrivers, returnFirstTwoDrivers){
+    return returnFirstTwoDrivers ( arrayOfDrivers)
+
+}
